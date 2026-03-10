dcdc50078a4b8e0d96e058cbae66794bf3c8dfde
MMW BOTZ <155316405+mmwbotzmain@users.noreply.github.com>
2026-03-10 22:05:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -424,174 +424,68 @@ async def cb_handler(client: Client, query: CallbackQuery):
             alert = alerts[int(i)]
             alert = alert.replace("\\n", "\n").replace("\\t", "\t")
             await query.answer(alert, show_alert=True)
-
-    elif query.data.startswith("file#"):
-        try:
-            ident, file_id = query.data.split("#", 1)
-        except ValueError:
-            return await query.answer("Invalid request", show_alert=True)
-
+    
+    elif query.data.startswith("file"):
+        ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
         if not files_:
-            return await query.answer("No such file exist.", show_alert=True)
-
-        # get_file_details returns a dict directly, not a list
-        files = files_
-        title = files.file_name
-        size = get_size(files.file_size)
+            return await query.answer('No such file exist.')
+        files = files_[0]
+        title = files.file_name        
+        size = get_size(files.file_size)   
         f_caption = files.file_name
-
-        settings = await get_settings(query.message.chat.id)
-
+        settings = await get_settings(query.message.chat.id)     
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption = CUSTOM_FILE_CAPTION.format(
-                    file_name=title or "",
-                    file_size=size or "",
-                    file_caption=f_caption or "",
-                    mention=query.from_user.mention
-                )
+                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption, mention=query.from_user.mention)
             except Exception as e:
                 logger.exception(e)
-
-        if not f_caption:
-            f_caption = title
-
+            f_caption = f_caption
+        if f_caption is None:
+            f_caption = f"{files.file_name}"
         try:
             if (AUTH_CHANNEL or REQ_CHANNEL) and not await is_subscribed(client, query):
-                return await query.answer(
-                    url=f"https://t.me/{temp.U_NAME}?start=file_{file_id}"
-                )
-
-            if settings.get("botpm"):
-                return await query.answer(
-                    url=f"https://t.me/{temp.U_NAME}?start=file_{file_id}"
-                )
-
-            return await query.answer(
-                url=f"https://t.me/{temp.U_NAME}?start=file_{file_id}"
-            )
-
+                await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+                return
+            elif settings['botpm']:
+                await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+                return
+            else:
+                await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+                return
         except UserIsBlocked:
-            await query.answer("Unblock the bot!", show_alert=True)
-
+            await query.answer('Unblock the bot mahn !', show_alert=True)
         except PeerIdInvalid:
-            await query.answer(
-                url=f"https://t.me/{temp.U_NAME}?start=file_{file_id}"
-            )
-
-        except Exception:
-            await query.answer(
-                url=f"https://t.me/{temp.U_NAME}?start=file_{file_id}"
-            )
-
-
-    elif query.data.startswith("files#"):
-        # Handler for pagination file buttons (files# prefix)
-        try:
-            ident, file_id = query.data.split("#", 1)
-        except ValueError:
-            return await query.answer("Invalid request", show_alert=True)
-
-        files_ = await get_file_details(file_id)
-        if not files_:
-            return await query.answer("No such file exist.", show_alert=True)
-
-        # get_file_details returns a dict directly, not a list
-        files = files_
-        title = files.file_name
-        size = get_size(files.file_size)
-        f_caption = files.file_name
-
-        settings = await get_settings(query.message.chat.id)
-
-        if CUSTOM_FILE_CAPTION:
-            try:
-                f_caption = CUSTOM_FILE_CAPTION.format(
-                    file_name=title or "",
-                    file_size=size or "",
-                    file_caption=f_caption or "",
-                    mention=query.from_user.mention
-                )
-            except Exception as e:
-                logger.exception(e)
-
-        if not f_caption:
-            f_caption = title
-
-        try:
-            if (AUTH_CHANNEL or REQ_CHANNEL) and not await is_subscribed(client, query):
-                return await query.answer(
-                    url=f"https://t.me/{temp.U_NAME}?start=files_{file_id}"
-                )
-
-            if settings.get("botpm"):
-                return await query.answer(
-                    url=f"https://t.me/{temp.U_NAME}?start=files_{file_id}"
-                )
-
-            return await query.answer(
-                url=f"https://t.me/{temp.U_NAME}?start=files_{file_id}"
-            )
-
-        except UserIsBlocked:
-            await query.answer("Unblock the bot!", show_alert=True)
-
-        except PeerIdInvalid:
-            await query.answer(
-                url=f"https://t.me/{temp.U_NAME}?start=files_{file_id}"
-            )
-
-        except Exception:
-            await query.answer(
-                url=f"https://t.me/{temp.U_NAME}?start=files_{file_id}"
-            )
-
-
-    elif query.data.startswith("checksub#"):
+            await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+        except Exception as e:
+            await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+    elif query.data.startswith("checksub"):
         if (AUTH_CHANNEL or REQ_CHANNEL) and not await is_subscribed(client, query):
-            return await query.answer(
-                "Join required channels first.", show_alert=True
-            )
-
-        try:
-            ident, file_id = query.data.split("#", 1)
-        except ValueError:
-            return await query.answer("Invalid request", show_alert=True)
-
+            await query.answer("I Like Your Smartness, But Don't Be Oversmart 😒", show_alert=True)
+            return
+        ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
         if not files_:
-            return await query.answer("No such file exist.", show_alert=True)
-
-        # get_file_details returns a dict directly, not a list
-        files = files_
+            return await query.answer('No such file exist.')
+        files = files_[0]
         title = files.file_name
         size = get_size(files.file_size)
         f_caption = files.file_name
-
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption = CUSTOM_FILE_CAPTION.format(
-                    file_name=title or "",
-                    file_size=size or "",
-                    file_caption=f_caption or "",
-                    mention=query.from_user.mention
-                )
+                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption, mention=query.from_user.mention)
             except Exception as e:
                 logger.exception(e)
-
-        if not f_caption:
-            f_caption = title
-
+                f_caption = f_caption
+        if f_caption is None:
+            f_caption = f"{title}"
         await query.answer()
-
         await client.send_cached_media(
             chat_id=query.from_user.id,
             file_id=file_id,
             caption=f_caption,
-            protect_content=True if ident == "checksubp" else False
+            protect_content=True if ident == 'checksubp' else False
         )
-
     elif query.data.startswith("killfilesdq"):
         ident, keyword = query.data.split("#")
         await query.message.edit_text(f"<b>Fᴇᴛᴄʜɪɴɢ Fɪʟᴇs ғᴏʀ ʏᴏᴜʀ ᴏ̨ᴜᴇʀʏ {keyword} ᴏɴ DB... Pʟᴇᴀsᴇ ᴡᴀɪᴛ...</b>")
