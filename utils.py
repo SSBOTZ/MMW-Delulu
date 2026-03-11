dfa4e215bee11b03ffd8a74dd654bd8026adc62b
MMW BOTZ <155316405+mmwbotzmain@users.noreply.github.com>
2026-03-11 20:19:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -165,7 +165,7 @@ async def get_poster(query, bulk=False, id=False, file=None):
         'url':f'https://www.imdb.com/title/tt{movieid}'
     }
 
-async def broadcast_messages(user_id, message):
+async def gbroadcast_messages(user_id, message):
     try:
         k = await message.copy(chat_id=user_id)
         await k.pin()
@@ -186,6 +186,28 @@ async def broadcast_messages(user_id, message):
         return False, "Error"
     except Exception as e:
         return False, "Error"
+
+async def broadcast_messages(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await broadcast_messages(user_id, message)
+    except InputUserDeactivated:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id}-Removed from Database, since deleted account.")
+        return False, "Deleted"
+    except UserIsBlocked:
+        logging.info(f"{user_id} -Blocked the bot.")
+        return False, "Blocked"
+    except PeerIdInvalid:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id} - PeerIdInvalid")
+        return False, "Error"
+    except Exception as e:
+        return False, "Error"
+
 
 async def search_gagala(text):
     headers = {
