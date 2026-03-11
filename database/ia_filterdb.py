75e1886badea18e67924641067d96e4920347cad
MMW BOTZ <155316405+mmwbotzmain@users.noreply.github.com>
2026-03-11 11:43:56 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -64,7 +64,7 @@ async def choose_mediaDB():
 
     db_map = {
         DATABASE_URI: Media,
-        SECONDDB_URI: Media2,
+        DATABASE_URI2: Media2,
         DATABASE_URI3: Media3,
         DATABASE_URI4: Media4,
         DATABASE_URI5: Media5,
