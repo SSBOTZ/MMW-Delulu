5967fe072c54ae7fef9878e1b19eb03dc84ed410
MMW BOTZ <155316405+mmwbotzmain@users.noreply.github.com>
2026-03-14 10:54:50 +0530

    Rename message to client in send_cached_media

--- plugins/commands.py
+++ plugins/commands.py
@@ -139,7 +139,7 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
-    ok = await message.send_cached_media(
+    ok = await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
